93456b7b14c273a3e90d3d3ac3cc5de1065f1ecd
MarcusMix <tradedomarcus@gmail.com>
2024-05-01 16:02:30 -0300

    feat: metodo getAll feito

--- CentralDasImagens/src/main/java/com/centraldasimagens/controller/FotoController.java
+++ CentralDasImagens/src/main/java/com/centraldasimagens/controller/FotoController.java
@@ -1,15 +1,28 @@
 package com.centraldasimagens.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.centraldasimagens.model.Foto;
+import com.centraldasimagens.model.FotoRepository;
+
 @RestController
 @RequestMapping("foto")
 public class FotoController {
+
+    @Autowired
+    private FotoRepository repository;
     
     @GetMapping
-    public void getAll() {
+    public List<Foto> getAll() {
+
+        List<Foto> listaFotos = repository.findAll();
+
+        return listaFotos;
 
     }
 }
