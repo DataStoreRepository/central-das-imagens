359737ff9dd9c213952b1cddef2812c798b65357
MarcusMix <tradedomarcus@gmail.com>
2024-05-01 15:17:47 -0300

    first commit

---
+++ CentralDasImagens/src/main/java/com/centraldasimagens/CentralDasImagensApplication.java
@@ -0,0 +1,13 @@
+package com.centraldasimagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDasImagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDasImagensApplication.class, args);
+	}
+
+}
---
+++ CentralDasImagens/src/test/java/com/centraldasimagens/CentralDasImagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centraldasimagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDasImagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
