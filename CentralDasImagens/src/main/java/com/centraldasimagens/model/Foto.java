2e2f494a40183de9a14856eb9b8e35ffbaef4902
MarcusMix <tradedomarcus@gmail.com>
2024-05-01 16:32:45 -0300

    feat: add os contrutores, getters e setters com loombok

--- CentralDasImagens/src/main/java/com/centraldasimagens/model/Foto.java
+++ CentralDasImagens/src/main/java/com/centraldasimagens/model/Foto.java
@@ -5,12 +5,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "foto")
-@Entity
+@Entity(name = "foto")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Foto {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String imagem;
