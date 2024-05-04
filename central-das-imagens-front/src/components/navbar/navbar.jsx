7c1013f22f3001495a6b1f93a34d215a2c201990
MarcusMix <tradedomarcus@gmail.com>
2024-05-04 15:13:15 -0300

    feat: navbar componente inicial

---
+++ central-das-imagens-front/src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css"
+
+function Navbar() {
+    return (
+      
+        <div className="topper">
+            <img src="" alt=""/>
+            <h1>Central das Imagens</h1>
+        </div>
+  
+    )
+}
+
+export default Navbar
