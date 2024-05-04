2a8f4a796b7d750336dd5c7bd55e90f43167858a
MarcusMix <tradedomarcus@gmail.com>
2024-05-04 17:17:02 -0300

    feat: navbar component

--- central-das-imagens-front/src/components/navbar/navbar.jsx
+++ central-das-imagens-front/src/components/navbar/navbar.jsx
@@ -3,10 +3,14 @@ import "./navbar.css"
 function Navbar() {
     return (
       
-        <div className="topper">
-            <img src="" alt=""/>
+        <nav className="topper">
+            <img src="" alt="Logo"/>
             <h1>Central das Imagens</h1>
-        </div>
+            <div className="pesquisar">
+                <input type="text" />   
+                <input type="submit" name="" id="" />
+            </div>
+        </nav>
   
     )
 }
