e7a239a443baebd5a6bb579a43dda4873ca2ed7e
MarcusMix <tradedomarcus@gmail.com>
2024-05-04 19:22:41 -0300

    feat: add logo

--- central-das-imagens-front/src/components/navbar/navbar.jsx
+++ central-das-imagens-front/src/components/navbar/navbar.jsx
@@ -1,14 +1,16 @@
 import "./navbar.css"
 
+import logo from "../../assets/logo.png"
+
 function Navbar() {
     return (
       
         <nav className="topper">
-            <img src="" alt="Logo"/>
+            <img src={logo} alt="Logo"/>
             <h1>Central das Imagens</h1>
             <div className="pesquisar">
-                <input type="text" />   
-                <input type="submit" name="" id="" />
+                <input placeholder="Procurar imagem..." type="text" name="text" className="input" />
+                <input type="submit" name="" id="" className="btn" />
             </div>
         </nav>
   
