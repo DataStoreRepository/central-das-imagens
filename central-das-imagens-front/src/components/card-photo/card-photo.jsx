8adfab74c2e1c0ca47ef9cdcf3bf0213161cd043
MarcusMix <tradedomarcus@gmail.com>
2024-05-04 15:13:40 -0300

    feat: card photo (demo)

---
+++ central-das-imagens-front/src/components/card-photo/card-photo.jsx
@@ -0,0 +1,15 @@
+import "./card-photo.css"
+
+function CardPhoto () {
+    return (
+        <>
+        <div className="card">
+            <img />
+            <h2></h2>
+            <h3><b>Autor: </b></h3>
+        </div>
+        </>
+    )
+}
+
+export default CardPhoto
