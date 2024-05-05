930d0d037e91c2784badb4ca26daab474a99177d
MarcusMix <tradedomarcus@gmail.com>
2024-05-05 12:30:14 -0300

    feat: config do react router

--- central-das-imagens-front/src/main.jsx
+++ central-das-imagens-front/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import CadastrarFoto from './pages/cadastrar-foto/cadastrar-foto.jsx'
+import Imagens from './pages/imagens/imagens.jsx'
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/cadastrar-foto",
+    element: <CadastrarFoto/>
+  },
+  {
+    path: "/imagens",
+    element: <Imagens/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
