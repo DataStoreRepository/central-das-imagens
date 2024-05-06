2c10d9396dc7f558aade2509886c5f72fdbee357
MarcusMix <tradedomarcus@gmail.com>
2024-05-06 20:08:49 -0300

    feat: metodo deletar foto por id

--- CentralDasImagens/src/main/java/com/centraldasimagens/controller/FotoController.java
+++ CentralDasImagens/src/main/java/com/centraldasimagens/controller/FotoController.java
@@ -1,10 +1,14 @@
 package com.centraldasimagens.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +47,19 @@ public class FotoController {
     }
 
     //delete e update
+    //PathVariable pela URL
+    //RequestBody pelo corpo
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFoto(@PathVariable Long id) {
+        Optional<Foto> fotoOptional = repository.findById(id); 
+
+        if (fotoOptional.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     
 }
