095c4b2c1d11127d964a6757ace28cb5d6558fc5
MarcusMix <tradedomarcus@gmail.com>
2024-05-06 20:30:15 -0300

    feat: add metodo save com id e body

--- CentralDasImagens/src/main/java/com/centraldasimagens/controller/FotoController.java
+++ CentralDasImagens/src/main/java/com/centraldasimagens/controller/FotoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +62,23 @@ public class FotoController {
         }
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Foto> updateFoto(@RequestBody Foto foto,
+                                           @PathVariable Long id) {
+
+        Optional<Foto> clienteOptional = repository.findById(id);
+
+        if(clienteOptional.isPresent()) {
+            //PathVariable
+            foto.setId(id);
+            
+            //RequestBody
+            repository.save(foto);
+
+            return ResponseEntity.ok(foto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
     
 }
