3c0550820d6cd93c95be28d0e90348ab7b323f68
MILENA CRISTINA DE JESUS <milena.jesus@alunos.sc.senac.br>
2024-05-07 20:17:36 -0300

    feat: criado usuario model e vinculo com o banco de dados

---
+++ CentralDasImagens/src/main/java/com/centraldasimagens/model/Usuario.java
@@ -0,0 +1,38 @@
+package com.centraldasimagens.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Table(name = "usuario")
+@Entity(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String senha;
+
+    @OneToMany
+    @JoinColumn(name = "id_usuario")
+    private List <Foto> fotos;
+}
