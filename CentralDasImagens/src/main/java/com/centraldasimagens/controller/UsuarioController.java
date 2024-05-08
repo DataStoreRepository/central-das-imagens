1d3ecd052ebe2220d5246f0a6219252600955684
MILENA CRISTINA DE JESUS <milena.jesus@alunos.sc.senac.br>
2024-05-07 21:27:46 -0300

    feat: adicionado metodo de listar todos os usuarios

--- CentralDasImagens/src/main/java/com/centraldasimagens/controller/UsuarioController.java
+++ CentralDasImagens/src/main/java/com/centraldasimagens/controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package com.centraldasimagens.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.centraldasimagens.model.Usuario;
 import com.centraldasimagens.model.UsuarioRepository;
 import com.centraldasimagens.model.UsuarioRequestDTO;
+import com.centraldasimagens.model.UsuarioRespostaDTO;
 
 @RestController
 @RequestMapping("/usuario")
@@ -17,6 +22,14 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
+    @GetMapping
+    public List<UsuarioRespostaDTO> getAll() {
+        List<UsuarioRespostaDTO> listaUsuarios = repository.findAll().stream().map(UsuarioRespostaDTO::new).toList();
+        
+        return listaUsuarios;
+    }
+    
+    @PostMapping
     public void saveUsuario(@RequestBody UsuarioRequestDTO dados) {
 
         Usuario dadosUsuario = new Usuario(dados);
