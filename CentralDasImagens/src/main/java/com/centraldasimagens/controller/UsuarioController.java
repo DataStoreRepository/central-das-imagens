a9ae3ddb7d60b75720b0a9719b50dfe861b55fb3
MILENA CRISTINA DE JESUS <milena.jesus@alunos.sc.senac.br>
2024-05-08 19:40:48 -0300

    feat: adicionado metodo de deletar e atualizar usuario

--- CentralDasImagens/src/main/java/com/centraldasimagens/controller/UsuarioController.java
+++ CentralDasImagens/src/main/java/com/centraldasimagens/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.centraldasimagens.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +40,29 @@ public class UsuarioController {
         Usuario dadosUsuario = new Usuario(dados);
 
         repository.save(dadosUsuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = repository.findById(id);
+
+        if(usuarioOptional.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioOptional = repository.findById(usuario.getId());
+        
+        if(usuarioOptional.isPresent()) {
+            repository.save(usuario);
+            return ResponseEntity.ok(usuario);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
