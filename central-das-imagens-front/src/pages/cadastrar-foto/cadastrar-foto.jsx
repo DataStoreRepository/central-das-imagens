9614ced01d5abaa644e87b14ea8b151b73e547d6
MarcusMix <tradedomarcus@gmail.com>
2024-05-12 15:35:53 -0300

    feat/style: ajuste da identação e métodos

--- central-das-imagens-front/src/pages/cadastrar-foto/cadastrar-foto.jsx
+++ central-das-imagens-front/src/pages/cadastrar-foto/cadastrar-foto.jsx
@@ -1,16 +1,67 @@
+import { useState } from "react";
 import { Input } from "../../components/input/input";
 
 function CadastrarFoto() {
+
+    const [titulo, setTitulo] = useState("")
+    const [descricao, setDescricao] = useState("")
+    const [imagem, setImagem] = useState("")
+
+    // const refreshPage = () => {
+	// 	document.location.reload();
+	// };
+
+    const API_URL = "http://localhost:8080/foto/"
+
+    const editarFoto = () => {
+		axios.post(API_URL + id, {
+			id: editValues.id,
+			name: editValues.name,
+			category: editValues.category,
+			cost: editValues.cost,
+		});
+
+	// 	handleClose();
+	// 	refreshPage();
+	};
+
+    // const handleClose = () => {
+	// 	props.setOpen!(false);
+	// };
+
+
     return (
         <div className="fora">
+            <h1>Alterar foto</h1>
             <div>
-                <Input type="text" text="Título da foto" name="name" placeholder="Insira título da foto..." />
+                <Input 
+                    type="text" 
+                    text="Título da foto" 
+                    name="name" 
+                    placeholder="Insira título da foto..." 
+                    value={titulo}
+                    onChangeFN={setTitulo}
+                />
             </div>
             <div>
-                <Input type="text" text="Descrição da foto" name="name" placeholder="Insira a descricao foto..." />
+                <Input 
+                    type="text" 
+                    text="Descrição da foto" 
+                    name="name" 
+                    placeholder="Insira a descricao foto..." 
+                    value={descricao}
+                    onChangeFN={setDescricao}
+                />
             </div>
             <div>
-                <Input type="text" text="Imagem" name="name" placeholder="Insira URL da foto" />
+                <Input 
+                    type="text" 
+                    text="Imagem" 
+                    name="name" 
+                    placeholder="Insira URL da foto" 
+                    value={imagem}
+                    onChangeFN={setImagem}
+                />
             </div>
         </div>
     )
