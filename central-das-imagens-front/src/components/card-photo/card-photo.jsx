6c4e970c3d91526d4343acc4f301fc8c8903757b
MarcusMix <tradedomarcus@gmail.com>
2024-05-12 15:34:26 -0300

    feat: link para página de atualização da foto

--- central-das-imagens-front/src/components/card-photo/card-photo.jsx
+++ central-das-imagens-front/src/components/card-photo/card-photo.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom"
 import "./card-photo.css"
+import { Button } from "../button/button"
 
 function CardPhoto (props) {
     return (
@@ -6,6 +8,7 @@ function CardPhoto (props) {
             <img src={props.foto} alt={props.foto}/>
             <h3>{props.titulo}</h3>
             <p>{props.descricao}</p>
+            <Button> <Link to={`/atualizar/${props.id}`}>Editar</Link> </Button>
         </div>
        
     )
