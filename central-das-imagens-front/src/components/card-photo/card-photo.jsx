4b79a253c02d391d012160c9934e8f62891cb0c3
MarcusMix <tradedomarcus@gmail.com>
2024-05-12 18:21:35 -0300

    feat: add modal and delete method

--- central-das-imagens-front/src/components/card-photo/card-photo.jsx
+++ central-das-imagens-front/src/components/card-photo/card-photo.jsx
@@ -1,18 +1,73 @@
-import { Link } from "react-router-dom"
 import "./card-photo.css"
+import { Link } from "react-router-dom"
 import { Button } from "../button/button"
 
 import { BsPencilSquare } from "react-icons/bs";
+import { FaTrash } from "react-icons/fa6";
+import { IoIosCloseCircle } from "react-icons/io";
+
+import { useNavigate } from "react-router-dom";
+
+
+import { useState } from "react";
+
+import axios from "axios"
 
 
 
 function CardPhoto (props) {
+
+    const navigate = useNavigate()
+
+    const [modal, setModal] = useState(false);
+    const API_URL = "http://localhost:8080/foto"
+
+    const toggleModal = () => {
+      setModal(!modal);
+    };
+  
+    if(modal) {
+      document.body.classList.add('active-modal')
+    } else {
+      document.body.classList.remove('active-modal')
+    }
+
+    const handleDelete = async () => {
+        try {
+            const response = await axios.delete(`${API_URL}/${props.id}`) 
+            response.status = 200
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+            console.log("Deu erro!")
+        }
+    }
+
     return (
         <div className="card">
             <img src={props.foto} alt={props.foto}/>
             <h3>{props.titulo}</h3>
             <p>{props.descricao}</p>
-            <Button> <Link className="link" to={`/atualizar/${props.id}`}> <BsPencilSquare /> Editar</Link> </Button>
+            <div className="botoes">
+                <Button> <Link className="link" to={`/atualizar/${props.id}`}> <BsPencilSquare /> Editar</Link> </Button>
+                <Button onClick={toggleModal}> <FaTrash /> Deletar</Button>
+            </div>
+            {modal && (
+            <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+
+                <h3> Tem certeza que deseja deletar?</h3>
+                <p>
+                    Depois de deletado será impossível voltar!
+                </p>
+                <button className="close-modal" onClick={toggleModal}>
+                    <IoIosCloseCircle size={20} />
+                </button>
+                <Button onClick={handleDelete}>Sim, tenho certeza!</Button>
+            </div>
+            </div>
+            )}
         </div>
        
     )
