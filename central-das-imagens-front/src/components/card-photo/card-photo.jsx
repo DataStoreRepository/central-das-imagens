2293050825eb01e1afe38079d557332fbd216677
MarcusMix <tradedomarcus@gmail.com>
2024-05-12 16:16:27 -0300

    style: add className to link and react icon

--- central-das-imagens-front/src/components/card-photo/card-photo.jsx
+++ central-das-imagens-front/src/components/card-photo/card-photo.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom"
 import "./card-photo.css"
 import { Button } from "../button/button"
 
+import { BsPencilSquare } from "react-icons/bs";
+
+
+
 function CardPhoto (props) {
     return (
         <div className="card">
             <img src={props.foto} alt={props.foto}/>
             <h3>{props.titulo}</h3>
             <p>{props.descricao}</p>
-            <Button> <Link to={`/atualizar/${props.id}`}>Editar</Link> </Button>
+            <Button> <Link className="link" to={`/atualizar/${props.id}`}> <BsPencilSquare /> Editar</Link> </Button>
         </div>
        
     )
