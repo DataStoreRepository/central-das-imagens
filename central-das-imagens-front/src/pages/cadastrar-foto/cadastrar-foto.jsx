e22368483452dda98b2d07c4e0a7e48b441b84de
MarcusMix <tradedomarcus@gmail.com>
2024-05-12 17:42:19 -0300

    feat: cadastrar foto completo

--- central-das-imagens-front/src/pages/cadastrar-foto/cadastrar-foto.jsx
+++ central-das-imagens-front/src/pages/cadastrar-foto/cadastrar-foto.jsx
@@ -1,46 +1,49 @@
 import { useState } from "react";
 import { Input } from "../../components/input/input";
+import { Button } from "../../components/button/button";
+import { useNavigate } from "react-router-dom";
+
+
+import axios from 'axios'
 
 function CadastrarFoto() {
 
-    const [titulo, setTitulo] = useState("")
-    const [descricao, setDescricao] = useState("")
-    const [imagem, setImagem] = useState("")
+    const navigate = useNavigate()
 
-    // const refreshPage = () => {
-	// 	document.location.reload();
-	// };
+    const [valores, setValores] = useState({
+        id: '',
+        descricao: '',
+        imagem: '',
+        titulo: ''
+    })
 
-    const API_URL = "http://localhost:8080/foto/"
+    const API_URL = "http://localhost:8080/foto"
 
-    const editarFoto = () => {
-		axios.post(API_URL + id, {
-			id: editValues.id,
-			name: editValues.name,
-			category: editValues.category,
-			cost: editValues.cost,
-		});
-
-	// 	handleClose();
-	// 	refreshPage();
-	};
-
-    // const handleClose = () => {
-	// 	props.setOpen!(false);
-	// };
-
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post(API_URL, valores);
+            response.status = 200
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+            console.log("Deu erro!")
+        }
+    }
 
     return (
         <div className="fora">
-            <h1>Alterar foto</h1>
+            <h1>Cadastrar foto</h1>
+
+            <form onSubmit={handleSubmit}>
             <div>
                 <Input 
                     type="text" 
                     text="Título da foto" 
                     name="name" 
                     placeholder="Insira título da foto..." 
-                    value={titulo}
-                    onChangeFN={setTitulo}
+                    value={valores.titulo}
+                    onChangeFN={e => setValores({...valores, titulo: e.target.value})}
                 />
             </div>
             <div>
@@ -49,8 +52,8 @@ function CadastrarFoto() {
                     text="Descrição da foto" 
                     name="name" 
                     placeholder="Insira a descricao foto..." 
-                    value={descricao}
-                    onChangeFN={setDescricao}
+                    value={valores.descricao}
+                    onChangeFN={e => setValores({...valores, descricao: e.target.value})}
                 />
             </div>
             <div>
@@ -59,10 +62,14 @@ function CadastrarFoto() {
                     text="Imagem" 
                     name="name" 
                     placeholder="Insira URL da foto" 
-                    value={imagem}
-                    onChangeFN={setImagem}
+                    value={valores.imagem}
+                    onChangeFN={e => setValores({...valores, imagem: e.target.value})}
                 />
+                <img className="image-update" src={valores.imagem} alt={valores.titulo}/>
             </div>
+            <Button type={"submit"}>Cadastrar foto</Button>
+            </form>
+            
         </div>
     )
 }
