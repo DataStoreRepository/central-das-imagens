0fd3519cb61fa1e0cadf416adc4e210bc5af9999
MARCUS VINÍCIUS SANDI <marcus.sandi@alunos.sc.senac.br>
2024-05-27 21:20:50 -0300

    feat: tela de usuarios

---
+++ central-das-imagens-front/src/pages/usuarios/usuarios.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+
+import Navbar from '../../components/navbar/navbar'
+import { Container } from '../../components/container/container'
+import { Loading } from '../../components/loading/loading'
+import { getUsuarioData } from '../../api/usuario'
+import { CardUser } from '../../components/card-user/card-user'
+
+
+export function Usuarios() {
+
+    const [usuario, setUsuario] = useState([])
+    const API_URL = "http://localhost:8080"
+
+    useEffect(() => {
+        getUsuarioData(API_URL, setUsuario)
+    }, [])
+
+
+    return (
+        <>
+        <Navbar/>
+        <table>
+       
+        <tbody>
+        {usuario.length === 0 ? <Loading/> : (
+        usuario.map((user,index) => (
+            <CardUser index={index} key={user.id} id={user.id} name={user.name} email={user.email}/>
+        ))
+        )}
+        </tbody>
+        </table>
+      </>
+    )
+}
