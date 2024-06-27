8f6af94aa47d06ae65b6abc60e006ed43e751ed7
MarcusMix <tradedomarcus@gmail.com>
2024-06-26 21:05:24 -0300

    feat: fix card-user

--- central-das-imagens-front/src/components/card-user/card-user.jsx
+++ central-das-imagens-front/src/components/card-user/card-user.jsx
@@ -3,6 +3,7 @@ import "./card-user.css"
 import { handleDelete } from "../../api/usuario";
 import { Link, useNavigate } from "react-router-dom";
 import { Toaster, toast } from 'sonner'
+import { Button } from "../button/button";
 
 
 export function CardUser(props) {
@@ -20,11 +21,10 @@ export function CardUser(props) {
             }, 1000);
         } catch (error) {
             console.log(error);
+            toast.error('Erro ao deletar!');
+        } finally {
+            toggleModal(); // Close the modal after attempting deletion
         }
-        //     toast.error('Erro ao deletar!');
-        // } finally {
-        //     toggleModal(); // Close the modal after attempting deletion
-        // }
     };
     
 
@@ -44,8 +44,8 @@ export function CardUser(props) {
                 <tr>
                     <td>{props.name}</td>
                     <td>{props.email}</td>
-                    <td className="gerenciar"><button class="edit-btn"> <Link  to={`/usuarios/atualizar/${props.id}`}>Editar</Link> </button></td>
-                    <td className="gerenciar"><button class="delete-btn" onClick={confirmDelete}>Deletar</button></td>
+                    <td className="gerenciar"><Button color="#2274A5"> <Link  className="link-" to={`/usuarios/atualizar/${props.id}`}>Editar</Link> </Button></td>
+                    <td className="gerenciar"><Button color="#E83F6F" onClick={confirmDelete}>Deletar</Button></td>
                 </tr>
             </tbody>
             </table>
