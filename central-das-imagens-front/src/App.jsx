52d976a6dbf7ec5e60402e3fa10479ca7668fa59
MarcusMix <tradedomarcus@gmail.com>
2024-06-26 21:02:44 -0300

    feat: remove buttons

--- central-das-imagens-front/src/App.jsx
+++ central-das-imagens-front/src/App.jsx
@@ -10,11 +10,6 @@ function App() {
   return (
     <div className='app'>
       <Navbar/>
-      {/* <h1>Bem Vindo</h1> */}
-      <Button> <Link to="/imagens" className='link'>Ir para todas as fotos </Link></Button>
-      <Button> <Link to="/cadastrar-foto" className='link'>Cadastrar Foto </Link></Button>
-      <Button> <Link to="/cadastrar-usuario" className='link'>Cadastrar Usuário</Link></Button>
-      <Button> <Link to="/usuarios" className='link'>Listar Usuários</Link></Button>
       <Imagens/>
     </div>
    
